76379f4fe912df9cd0591840de7324a3d7347022
anasmef <anasmeftah197@gmail.com>
2024-01-27 04:38:21 +0100

    card partie

--- src/Component/2-hero/hero.jsx
+++ src/Component/2-hero/hero.jsx
@@ -9,7 +9,7 @@ export default function Hero() {
           <div className='divP'><img className='moi' src="./public/profil.jpg" alt="" /></div>
           <div className='divP'><h1>Full Stack Developper</h1></div>
           <div className='divP'><p>Hello, I'm Anas Meftah, a skilled Full Stack Developer with a passion for crafting robust and user-friendly web applications. With expertise in both front-end and back-end technologies, I am adept at handling the entire development process from start to finish.</p></div>
-          <div className='divP '>
+          <div className='divP flex'>
             <div className='icons flex'>
               <div className='icon-x icon'></div>
               <div className='icon-github icon'></div>
--- src/Component/3-main/main.jsx
+++ src/Component/3-main/main.jsx
@@ -2,6 +2,75 @@ import React from 'react'
 import  './main.css'
 export default function Main() {
   return (
-    <div>main</div>
+    <>
+      <main className='flex'>
+        <section className='left'>
+        <div className='allBtn flex'>
+          <div>
+              <button className='active btnPrj  '>all project</button>
+          </div>
+
+          <div>
+              <button className='btnPrj'>html & css</button>
+          </div>
+          <div >
+              <button className='btnPrj'>javascript</button>
+          </div>
+          <div>
+              <button className='btnPrj'>react</button>
+          </div>
+          <div>
+              <button className='btnPrj'>laravel</button>
+          </div> 
+        </div>  
+        
+        </section>
+
+          
+        <section className='right flex'>
+          <article className='card'>
+            <div className='image'><img  src="./public/profil.jpg" alt="" /></div>
+            <h1 className='title'>titre project</h1>
+            <p className='paragraphe'>
+              dddddddddddddddddddddddd
+              fffffffffffffffffffffffff
+              ggggggggggggggggggggggg
+              hhhhhhhhhhhhhhhhh
+            </p>
+          </article>
+          <article className='card'>
+            <div className='image'><img  src="./public/profil.jpg" alt="" /></div>
+            <h1 className='title'>titre project</h1>
+            <p className='paragraphe'>
+              dddddddddddddddddddddddd
+              fffffffffffffffffffffffff
+              ggggggggggggggggggggggg
+              hhhhhhhhhhhhhhhhh
+            </p>
+          </article>
+          <article className='card'>
+            <div className='image'><img  src="./public/profil.jpg" alt="" /></div>
+            <h1 className='title'>titre project</h1>
+            <p className='paragraphe'>
+              dddddddddddddddddddddddd
+              fffffffffffffffffffffffff
+              ggggggggggggggggggggggg
+              hhhhhhhhhhhhhhhhh
+            </p>
+          </article>
+          <article className='card'>
+            <div className='image'><img  src="./public/profil.jpg" alt="" /></div>
+            <h1 className='title'>titre project</h1>
+            <p className='paragraphe'>
+              dddddddddddddddddddddddd
+              fffffffffffffffffffffffff
+              ggggggggggggggggggggggg
+              hhhhhhhhhhhhhhhhh
+            </p>
+          </article>
+        </section>
+      </main>
+      <div className='header-border'/>
+    </>
   )
 }
